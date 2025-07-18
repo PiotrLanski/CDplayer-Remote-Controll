e2c06a12195a6d1f55c73a95f321ea8101975b06
Piotrek <lanski.piotr@gmail.com>
2025-07-18 12:46:30 +0200

    first echo server configuration

--- server/main.go
+++ server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
 
 func main() {
 	fmt.Println("cdPlayer")
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			log.Fatal(err)
+		}
+		go func(c net.Conn) {
+			io.Copy(c, c)
+			c.Close()
+		}(conn)
+		//go handleConnection(conn)
+	}
 }
